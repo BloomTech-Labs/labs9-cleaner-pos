0bdb685669a7f811a22c5d114418fbb691521ea9
Chris Honda <christopher.honda92@gmail.com>
2019-01-09 14:06:05 -0800

    Created test files for shared components

---
+++ frontend/src/components/__tests__/Button.spec.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'jest';
+
+import Button from '../shared_components/Button';
+
---
+++ frontend/src/components/__tests__/Sidebar.spec.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+
+import Sidebar from '../shared_components/Sidebar';
