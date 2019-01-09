9f2794ec102d7a92c04797d497547a42ec89640b
Mister-Corn <code@ronaldlibago.com>
2019-01-08 22:10:44 -0800

    Separate data from seed generator
    Data to be seeded is stored in a separate file apart from the seed file itself. This is to allow
    the data to be used both for seeding and for tests which test against the seeded DB.

---
+++ backend/data/seeds/users.js
@@ -0,0 +1,11 @@
+const data = require('./data/usersData');
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('user')
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex('user').insert(data);
+    });
+};
