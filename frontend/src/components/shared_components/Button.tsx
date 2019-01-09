bca56a375337ee8ce896165f4852ef9cdc45d6b8
kelly <kelfro@fastmail.com>
2019-01-09 13:55:17 -0800

    sketch out model for Button & Sidebar

--- frontend/src/components/shared_components/Button.tsx
+++ frontend/src/components/shared_components/Button.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-export const Button = () => {
-  return <div />;
+const Button = (props) => {
+    return (
+        <button onClick={ props.onClick }className={ `${ props.style }` }>
+            {props.action}
+        </button>
+    );
 };
+
+Button.defaultProps = {action: '&'};
+
+export default Button; 
+
--- frontend/src/components/shared_components/Sidebar.tsx
+++ frontend/src/components/shared_components/Sidebar.tsx
@@ -1,5 +1,69 @@
 import React from "react";
 
-export const Button = () => {
-  return <div />;
-};
+import React, { Component } from 'react';
+//import { Link } from 'react-router-dom';
+//import './index.css';
+
+class Sidebar extends Component {
+ render() {
+  return(
+   <nav>
+    <div>
+     <h3>Guests</h3>
+    </div>
+    <div>
+     <Link to='/'>
+      <h3>Checkout</h3>
+     </Link>
+    </div>
+    <br />
+	<div>
+	  <h3>------</h3>
+	</div>
+    <div>
+     <Link to='/'>
+      <h3>Houses</h3>
+     </Link>
+    </div>
+    <div>
+     <Link to='/'>
+      <h3>Assistants</h3>
+     </Link>
+    </div>
+    <div>
+     <Link to='/'>
+      <h3>Reports</h3>
+     </Link>
+    </div>
+    <div>
+     <Link to='/'>
+      <h3>Settings</h3>
+     </Link>
+    </div>
+    <div>
+     <Link to='/'>
+      <h3>Billing</h3>
+     </Link>
+    </div>
+   </nav>
+)
+}
+}
+
+export default Sidebar;
+
+
+
+
+
+
+
+
+
+
+   </nav>
+  )
+ }
+}
+
+export default Sidebar;
