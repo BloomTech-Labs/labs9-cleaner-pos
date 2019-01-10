f1cf0fd7333a50bfc50c597d79cb203d07b5f4ab
William VanDolah <william.vandolah@gmail.com>
2019-01-09 20:55:17 -0600

    started on tests

--- backend/data/seeds/data/housesData.js
+++ backend/data/seeds/data/housesData.js
@@ -1,21 +1,21 @@
 module.exports = [
   {
     name: 'house name 1',
-    address: '123 go fuck yourself ave',
+    address: '123 go duck yourself ave',
     price: 450.5,
     cleaning_fee: 65,
     extra_guest_fee: 30,
   },
   {
     name: 'house name 2',
-    address: '123 go fuck yourself st',
+    address: '123 go duck yourself st',
     price: 450.5,
     cleaning_fee: 65,
     extra_guest_fee: 30,
   },
   {
     name: 'house name 3',
-    address: '123 go fuck yourself ln',
+    address: '123 go duck yourself ln',
     price: 450.5,
     cleaning_fee: 65,
     extra_guest_fee: 30,
