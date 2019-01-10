13b32590769d1660f1bbc91d2f71946e9e7bcca0
Chris Honda <christopher.honda92@gmail.com>
2019-01-10 13:53:03 -0800

    Basic nav on Landing page; Landing imported and mounted to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import { RouteProps } from 'react-router';
+import { Route, RouteProps } from 'react-router';
 import Login from './pages/Login';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './components/shared_components/Button';
+import Landing from './pages/Landingpage';
+
 const App = (props: RouteProps) => {
   return (
     <div className='App'>
-      <Login />
+      <Route exact path='/' component={ Landing } />
     </div>
-  );
-};
+  )
+}
 
 export default App;
--- frontend/src/pages/Landingpage.tsx
+++ frontend/src/pages/Landingpage.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
+import { RouteProps } from 'react-router';
 import styled from '@emotion/styled';
 
-const Container = styled.div`
+import Button from '../components/shared_components/Button';
+
+const Container = styled('div')`
   background-color: burlywood;
+  `
+const Nav = styled('nav')`
+  margin: 0 auto;
+  max-width: 880px;
+  display: flex;
+  justify-content: space-between;
 `
 
 type Props = {};
@@ -10,8 +19,21 @@ type Props = {};
 //   return <div />;
 // };
 
-const Landing = (props) => {
-
-}
+const Landing = (props: RouteProps) => {
+  return (
+    <div>
+      <Container>
+        <h1>Cleaner POS</h1>
+        <h4>A POS that helps you keep things clean</h4>
+        <Nav>
+          <a href='/'>Home</a>
+          <a href='/'>About</a>
+          <a href='/'>Contact</a>
+          <Button text='Sign In'/>
+        </Nav>
+      </Container>
+    </div>
+  );
+};
 
 export default Landing;
