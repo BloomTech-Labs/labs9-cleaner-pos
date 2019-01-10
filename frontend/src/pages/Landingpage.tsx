6fbcb26616d93baf28969614872fff1779c6a11e
Chris Honda <christopher.honda92@gmail.com>
2019-01-10 14:36:10 -0800

    Added image; reformat nav layout

--- frontend/src/pages/Landingpage.tsx
+++ frontend/src/pages/Landingpage.tsx
@@ -3,15 +3,31 @@ import { RouteProps } from 'react-router';
 import styled from '@emotion/styled';
 
 import Button from '../components/shared_components/Button';
+import house from '../assets/house.jpg';
 
 const Container = styled('div')`
-  background-color: burlywood;
+  background-color: gray;
+  padding: 20px;
   `
+
 const Nav = styled('nav')`
   margin: 0 auto;
+  padding: 20px;
   max-width: 880px;
   display: flex;
   justify-content: space-between;
+`
+
+const Wrapper = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const CenterImg = styled('img')`
+  max-height: 500px;
+  max-width: 800px;
+  border-radius: 100px;
 `
 
 type Props = {};
@@ -31,6 +47,11 @@ const Landing = (props: RouteProps) => {
           <a href='/'>Contact</a>
           <Button text='Sign In'/>
         </Nav>
+        <Wrapper>
+          <h3>Left</h3>
+            <CenterImg src={house} alt='Beautiful house with a pool' />
+          <h3>Right</h3>
+        </Wrapper>
       </Container>
     </div>
   );
