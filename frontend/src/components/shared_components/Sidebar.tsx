b6fede285f8e0a019a8a0244db1d48eb1c2fdfd2
kelly <kelfro@fastmail.com>
2019-01-10 13:15:42 -0800

    updated to remove react fragments

--- frontend/src/components/shared_components/Sidebar.tsx
+++ frontend/src/components/shared_components/Sidebar.tsx
@@ -9,45 +9,45 @@ import Button from './Button'
 const Sidebar = (props) => {
 	return(
    		<nav>
-    		<>
+    		<div>
      			<Link to = '/guests'>
 					<Button>Guests</Button>
 				</Link>
-			</>
-    		<>
+			</div>
+    		<div>
 				<Link to = '/checkout'>
       				<Button>Checkout</Button>
     			</Link>
-			</>
+			</div>
     		<br />
-			<>
+			<div>
 	  			<Button>------</Button>
-			</>
-    		<>
+			</div>
+    		<div>
 				<Link to = '/houses'>
       				<Button>Houses</Button>
 				</Link>
-    		</>
-    		<>
+    		</div>
+    		<div>
 				<Link to = '/assistants'>
       				<Button>Assistants</Button>
 				</Link>
-    		</>
-    		<>
+    		</div>
+    		<div>
 				<Link to = '/reports'>
       				<Button>Reports</Button>
     			</Link>
-			</>
-    		<>
+			</div>
+    		<div>
 				<Link to = '/settings'>
       				<Button>Settings</Button>
     			</Link>
-			</>
-    		<>
+			</div>
+    		<div>
 				<Link to = '/billing'>
       				<Button>Billing</Button>
 				</Link>
-    		</>
+    		</div>
 
    		</nav>
 	)
