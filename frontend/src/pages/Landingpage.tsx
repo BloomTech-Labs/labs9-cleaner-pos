680f3062935d8bfea968b2ea4d421ccf9fd8cab2
Chris Honda <christopher.honda92@gmail.com>
2019-01-09 21:06:55 -0800

    Added Emotion packages and started bare bones of landing page

--- frontend/src/pages/Landingpage.tsx
+++ frontend/src/pages/Landingpage.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  background-color: burlywood;
+`
 
 type Props = {};
-export const Button = () => {
-  return <div />;
-};
+// export const Button = () => {
+//   return <div />;
+// };
+
+const Landing = (props) => {
+
+}
+
+export default Landing;
