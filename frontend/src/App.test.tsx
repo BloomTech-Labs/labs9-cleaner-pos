a73cf16453d5ebb78c8a2605399de3b2f4824205
NandoTheessen <nando.theessen@gmail.com>
2019-01-10 02:40:33 +0100

    Add jest.mock for app.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('react-firebaseui/StyledFirebaseAuth', () => () => {
+  return <div>Hellooo</div>;
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
