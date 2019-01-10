abfdf396be377d98f9d22321490ff99e704f0599
Chris Honda <christopher.honda92@gmail.com>
2019-01-09 16:53:03 -0800

    Changed suffix of Sidebar test file in attempt to pass Travis CI test

--- frontend/src/components/__tests__/Button.spec.tsx
+++ frontend/src/components/__tests__/Button.spec.tsx
@@ -4,9 +4,10 @@ import 'jest';
 
 import Button from '../shared_components/Button';
 
+// Test Definition: The button element must render without crashing
 describe('<Button />', () => {
     it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<Button />, div);
+        const div = document.createElement('div');
+        ReactDOM.render(<Button />, div);
     });
-  });
+});
