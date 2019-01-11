f25d4534abaac3c284042c854582cb88d13c4777
Chris Honda <christopher.honda92@gmail.com>
2019-01-10 16:27:23 -0800

    Made Landing page default route; set Login path and component; Page renders correctly but Login component does not render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,8 @@ const App = (props: RouteProps) => {
   return (
     <div className='App'>
       <Switch>
-        <Route path='/' component={Login} />
+        <Route path='/' component={Landing} />
+        <Route path='/Login' component={Login} />
         <Route exact path='/postreg' component={PostRegister} />
       </Switch>
     </div>
