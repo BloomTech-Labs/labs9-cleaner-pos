86ea440d85e99313078e9bad842687542de48271
William VanDolah <william.vandolah@gmail.com>
2019-01-11 16:07:14 -0600

    removed null constraint was not playing nice with sqlite tests

--- backend/data/migrations/20190108161537_item_complete2.js
+++ backend/data/migrations/20190108161537_item_complete2.js
@@ -14,10 +14,7 @@ exports.up = function(knex, Promise) {
       .foreign('stay_id')
       .references('stay.id')
       .onDelete('CASCADE');
-    table
-      .boolean('complete')
-      .notNullable()
-      .defaultTo(false);
+    table.boolean('complete').defaultTo(false);
   });
 };
 
