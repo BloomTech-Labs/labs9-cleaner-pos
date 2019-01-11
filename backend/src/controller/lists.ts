043c73aee3c034efa3fa4f6a39e4cc81d0a932f4
William VanDolah <william.vandolah@gmail.com>
2019-01-11 10:11:21 -0600

    added check to see if valid house id was given

--- backend/src/controller/lists.ts
+++ backend/src/controller/lists.ts
@@ -1,9 +1,14 @@
 import { findLists } from '../models/lists';
 import { Request, Response, NextFunction } from 'express';
+import { findHouse } from '../models/houses';
 
 export const get = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { houseId } = req.params;
+    const isHouse = await findHouse(houseId);
+    if (!isHouse) {
+      throw Error('an invalid house id was given');
+    }
     const lists = await findLists(houseId);
     res.status(200).json(lists);
   } catch (e) {
