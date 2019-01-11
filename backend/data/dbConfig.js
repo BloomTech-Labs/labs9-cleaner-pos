1b96a3f6f3b5e85c95f9efa5b8df93fef863f467
NandoTheessen <nando.theessen@gmail.com>
2019-01-11 02:26:22 +0100

    Change webpackconfig to use Dotenv module for production

--- backend/data/dbConfig.js
+++ backend/data/dbConfig.js
@@ -3,7 +3,7 @@ const knexConfig = require('../knexfile');
 
 // Change config file based on environment
 // default to 'development' if no .env
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 console.log('env for dbconfig', environment, process.env.NODE_ENV);
 
 module.exports = knex(knexConfig[environment]);
