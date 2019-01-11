7f52ba25005d8c3fbb2eb6bfdde5b2eb59664d96
William VanDolah <william.vandolah@gmail.com>
2019-01-11 13:47:05 -0600

    added seed data for items complete

---
+++ backend/data/seeds/data/itemCompleteData.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    item_id: 1,
+    stay_id: 1,
+  },
+  {
+    item_id: 4,
+    stay_id: 1,
+  },
+  {
+    item_id: 7,
+    stay_id: 1,
+  },
+  {
+    item_id: 8,
+    stay_id: 1,
+  },
+  {
+    item_id: 13,
+    stay_id: 1,
+  },
+  {
+    item_id: 16,
+    stay_id: 1,
+  },
+  {
+    item_id: 19,
+    stay_id: 1,
+  },
+  {
+    item_id: 20,
+    stay_id: 1,
+  },
+];
