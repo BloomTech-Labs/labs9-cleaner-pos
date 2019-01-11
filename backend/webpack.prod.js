f95da7f5bd075418adbc5d82e38cac93873ab308
NandoTheessen <nando.theessen@gmail.com>
2019-01-11 02:41:40 +0100

    Add webpack config for production

--- backend/webpack.prod.js
+++ backend/webpack.prod.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-const isProduction = true;
 // Common plugins
 let plugins = [new webpack.NamedModulesPlugin()];
 
