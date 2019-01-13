f4123a3a37159b74141ddc5a546ef8bab5264574
Mister-Corn <code@ronaldlibago.com>
2019-01-12 17:14:45 -0800

    Develop first-pass Formik form with validation
    * @types/yup was added as a depedency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { RouteProps, Route, Switch } from 'react-router';
 import Login from './pages/Login';
 import PostRegister from './pages/PostRegister';
+import PostForm from './pages/PostForm';
 import './App.css';
 import Sidebar from './components/shared_components/Sidebar';
-
-
 
 import Button from './components/shared_components/Button';
 import Landing from './pages/Landingpage';
@@ -16,10 +15,10 @@ const App = (props: RouteProps) => {
       <Switch>
         <Route exact path='/' component={Landing} />
         <Route path='/Login' component={Login} />
-        <Route exact path='/postreg' component={PostRegister} />
+        <Route exact path='/postreg' component={PostForm} />
       </Switch>
     </div>
-  )
-}
+  );
+};
 
 export default App;
