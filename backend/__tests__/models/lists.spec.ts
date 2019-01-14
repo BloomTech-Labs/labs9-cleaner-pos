e2acd2f9f9b3a4d577bf991056fe7cfbaa905fbb
William VanDolah <william.vandolah@gmail.com>
2019-01-14 13:18:30 -0600

    added get tests for db list

---
+++ backend/__tests__/models/lists.spec.ts
@@ -0,0 +1,56 @@
+import knex from 'knex';
+import knexConfig from '../../knexfile';
+import {
+  findLists,
+  findListsStay,
+  postList,
+  putList,
+  deleteList,
+  justListsByHouse,
+} from '../../src/models/lists';
+import { List } from '../../src/interface';
+import data from '../../data/seeds/data/listsData';
+// Mock db in users model functions
+jest.mock('../../data/dbConfig');
+import db from '../../data/dbConfig';
+
+// Use testDb instead of DB defined in env
+// TODO: Find way to define type for mockImplementation
+// spyon is a lead, but only works on a particular method?
+const testDb = knex(knexConfig.test);
+// @ts-ignore
+db.mockImplementation((table: string) => testDb(table));
+const newHouse: List = {
+  house_id: 1,
+  type: 'before',
+};
+
+describe('List DB functions', () => {
+  beforeAll(async () => {
+    try {
+      await testDb.migrate.rollback();
+      await testDb.migrate.latest();
+      await testDb.seed.run();
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  test('Find Lists', async () => {
+    try {
+      const find = await findLists(1);
+      expect(Object.keys(find)[0]).toBe('before');
+    } catch (e) {
+      throw Error(e);
+    }
+  });
+
+  test('Find Lists for stay', async () => {
+    try {
+      const find: any = await findListsStay(1, 1);
+      expect(Object.keys(find)[0]).toBe('befores');
+    } catch (e) {
+      throw Error(e);
+    }
+  });
+});
