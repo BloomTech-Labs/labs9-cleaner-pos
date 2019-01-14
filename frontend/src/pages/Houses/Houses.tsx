8815f1bba790054f944b0d0affe0d6363e163691
NandoTheessen <nando.theessen@gmail.com>
2019-01-14 23:32:47 +0100

    Adds Houses.styling.tsx containing all styled components used in Houses.tsx

--- frontend/src/pages/Houses/Houses.tsx
+++ frontend/src/pages/Houses/Houses.tsx
@@ -1,7 +1,17 @@
 import React, { useState, useLayoutEffect, useRef } from 'react';
-import { Button } from '../components/shared_components/index';
+import { Button } from '../../components/shared_components/index';
 import axios from 'axios';
-import styled from '@emotion/styled';
+import {
+  HousesContainer,
+  HouseItem,
+  CardBody,
+  ThumbNail,
+  CardContent,
+  ButtonContainer,
+  CardHeading,
+  Cleaner,
+  CheckList,
+} from './Houses.styling';
 
 interface HousesEnum extends Array<House> {}
 
@@ -17,98 +27,6 @@ interface House {
   guest_guide?: any;
   ast_guide?: any;
 }
-
-const cardHeight = 216;
-
-const HouseItem = styled('div')`
-  max-width: ${1136 * 0.9}px;
-  height: ${cardHeight}px;
-  border-radius: 0px;
-  padding-left: 1px;
-  margin-top: 24px;
-  display: flex;
-  text-align: left;
-  border: 0.5px solid black;
-  -webkit-box-shadow: 0 8px 17px 2px rgba(0, 0, 0, 0.14),
-    0 3px 14px 2px rgba(0, 0, 0, 0.12), 0 5px 5px -3px rgba(0, 0, 0, 0.2);
-  box-shadow: 0 8px 17px 2px rgba(0, 0, 0, 0.14),
-    0 3px 14px 2px rgba(0, 0, 0, 0.12), 0 5px 5px -3px rgba(0, 0, 0, 0.2);
-`;
-
-const ButtonContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-
-const ThumbNail = styled('img')`
-  width: ${cardHeight - 1}px;
-  height: ${cardHeight - 1}px;
-`;
-
-const CardHeading = styled('div')`
-  margin-top: 12px;
-  height: 76px;
-  h4 {
-    margin: 0;
-    font-family: Roboto;
-    font-weight: bold;
-    font-size: 30px;
-  }
-  p {
-    font-weight: light;
-    font-size: 16px;
-    margin-block-start: 0;
-    margin-block-end: 0;
-  }
-`;
-const CardContent = styled('div')`
-  padding: 0 15px;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
-
-const CardBody = styled('div')`
-  display: flex;
-  justify-content: space-around;
-`;
-
-const CheckList = styled('div')`
-  text-align: center;
-  -moz-box-shadow: 0 0 3px #000;
-  -webkit-box-shadow: 0 0 3px #000;
-  box-shadow: 0 0 3px #000;
-  padding: 0px 15px;
-  font-size: 24px;
-  font-weight: light;
-  p {
-    margin: auto;
-    font-weight: bold;
-  }
-`;
-
-const Cleaner = styled('div')`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const HousesContainer = styled('div')`
-  font-family: roboto;
-  position: relative;
-  width: 70%;
-  margin: 0 auto;
-  padding-top: 48px;
-  span {
-    font-size: 36px;
-    position: absolute;
-    top: 0;
-    left: 0;
-    text-align: left;
-    border-bottom: 1px solid #b8003f;
-  }
-`;
 
 const Houses = () => {
   const [houses, setHouses] = useState<HousesEnum>([]);
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Houses from './Houses';
+import Houses from './Houses/Houses';
 import Login from './Login';
 import LandingPage from './Landingpage';
 import PostRegister from './PostRegister';
