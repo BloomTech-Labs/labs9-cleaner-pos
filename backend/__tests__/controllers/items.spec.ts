5d3689fb228e5b827b0b74b5f8e8ade99b33bc08
William VanDolah <william.vandolah@gmail.com>
2019-01-14 17:01:02 -0600

    item controller get test

---
+++ backend/__tests__/controllers/items.spec.ts
@@ -0,0 +1,50 @@
+import 'jest';
+import request from 'supertest';
+import app from '../../src/app';
+
+import knex from 'knex';
+import knexConfig from '../../knexfile';
+
+// Mock db in users model functions
+jest.mock('../../data/dbConfig');
+import db from '../../data/dbConfig';
+
+const testDb = knex(knexConfig.test);
+// @ts-ignore
+db.mockImplementation((table: string) => testDb(table));
+
+describe('/item routes', () => {
+  beforeAll(async () => {
+    try {
+      await testDb.migrate.rollback();
+      await testDb.migrate.latest();
+      await testDb.seed.run();
+    } catch (err) {
+      throw err;
+    }
+  });
+
+  test('Able to get all lists', (done) => {
+    request(app)
+      .get('/items')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body).toBe('object');
+        expect(Object.keys(body[0])).toHaveLength(3);
+        done();
+      });
+  });
+
+  test('Able to get single lists', (done) => {
+    request(app)
+      .get('/items/1')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body).toBe('object');
+        expect(Object.keys(body[0])).toHaveLength(3);
+        done();
+      });
+  });
+});
