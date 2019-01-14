1c58ceab64953f7665edeb7700ef4638f1c3c451
kelly <kelfro@fastmail.com>
2019-01-14 15:19:16 -0800

    updated tsconfig.json to set 'noImplicitAny' to false so I can run event handlers, worked more on react hooks for billing.tsx

--- frontend/src/pages/Billing.tsx
+++ frontend/src/pages/Billing.tsx
@@ -3,37 +3,49 @@ import { RouteComponentProps } from 'react-router';
 import Sidebar from '../components/shared_components/Sidebar';
 import styled from '@emotion/styled';
 
-const Main = styled.div`
+const Main = styled('div')`
 	margin: 50px
 `
+function Billing(props: RouteComponentProps) {
+	const [info, setValues] = useState({
+		ccnum:'',
+		exp: '',
+		cvv: ''
+ });
+	const printValues = e => {
+		e.preventDefault();
+		console.log(info.ccnum, info.exp, info.cvv);
 
-function Billing {
+	};
+
+	const handleSubmit = e => {
+		setValues ({
+			...info,
+			[e.target.name]: e.target.value
+		});
+	};
+
+//const Billing = (props: RouteComponentProps) => {
 //	handleChange(e) {
 //		this.setState({
 //			[name].value
 //		});
 //	}
-
-	const [ ccnum, setCCNum ] = useState("");
+//	const [ ccnum, setCCNum ] = useState(0);
 	return(
 		<>
-		<div>
-			<Sidebar />
-		</div>
 		<Main>
 			
 			<div>
 			{/*Create form for collecting billing information - CC#, Exp. CVV [submit button]*/}
-			<form>
+			<form onSubmit={ printValues }>
 				<label>
-{/*					<input name="CC Number" type="string" onClick={() => {
-						const NewCCN = prompt("Enter your CC number");
-						setCCNum(NewCCN);
-					}}
-					/>
-*/}					<input name="Expiration" type="number"/>
-					<input name="CVV" type="number"/>
+					<input name="CC Number" value={ info.ccnum } onClick= { handleSubmit }/>
+					<input name="Expiration" value={ info.exp } onClick= { handleSubmit }/>
+					<input name="CVV" value={ info.cvv } onClick= { handleSubmit }/>
 				</label>
+				<br />
+				<button>Submit</button>
 			</form>
 			{/* Route billing information through stripe*/}
 			{/* Once processed, activate subscription? create confirmation code?*/}
