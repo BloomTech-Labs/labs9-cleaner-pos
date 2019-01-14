9c830f554807c9c428e56ee8e5c2d280c8cfbe3b
kelly <kelfro@fastmail.com>
2019-01-14 10:15:01 -0800

    adding payment form to billing page

--- frontend/src/pages/Billing.tsx
+++ frontend/src/pages/Billing.tsx
@@ -4,13 +4,27 @@ import Sidebar from '../components/shared_components/Sidebar';
 import styled from '@emotion/styled';
 
 const Billing = (props: RouteComponentProps) => {
+//	handleChange(e) {
+//		this.setState({
+//			[name].value
+//		});
+//	}
 	return(
 		<>
 		<div>
 			<Sidebar />
 		</div>
 		<div>
-			<p>Billing stuff</p>
+			{/*Create form for collecting billing information - CC#, Exp. CVV [submit button]*/}
+			<form>
+				<label>
+					<input name="CC Number" type="number"/>
+					<input name="Expiration" type="number"/>
+					<input name="CVV" type="number"/>
+				</label>
+			</form>
+			{/* Route billing information through stripe*/}
+			{/* Once processed, activate subscription? create confirmation code?*/}
 		</div>
 		</>
 	)
