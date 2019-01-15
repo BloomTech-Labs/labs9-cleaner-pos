b14b216a223bac37ba330f6c7ddb073f86880e42
NandoTheessen <nando.theessen@gmail.com>
2019-01-15 03:21:19 +0100

    Remove unnecessary dependencies

--- frontend/src/pages/Houses/HouseDetails.tsx
+++ frontend/src/pages/Houses/HouseDetails.tsx
@@ -4,7 +4,7 @@ import { Container } from '../../components/shared_components/index';
 export const HouseDetails = () => {
   return (
     <Container>
-      <h1>Hello World!</h1>
+      <h1>Hello World!2</h1>
     </Container>
   );
 };
