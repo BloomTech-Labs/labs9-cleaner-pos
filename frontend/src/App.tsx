a568787c872f6f798aa12a87df76877dae30fa20
NandoTheessen <nando.theessen@gmail.com>
2019-01-16 01:31:11 +0100

    First pass stripe checkout implementation - Adds react-stripe-checkout - Removes React-stripe-elements

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,23 +5,13 @@ import './App.css';
 import { StripeProvider } from 'react-stripe-elements';
 import Sidebar from './components/shared_components/Sidebar';
 import Button from './components/shared_components/Button';
-import MyStoreCheckout from './stripeTest/MyStoreCheckout';
+import Stripe from './stripe';
 
 const App = (props: RouteProps) => {
-  const { REACT_APP_stripe_API } = process.env;
   return (
     <div className='App'>
       <Switch>
-        <Route
-          exact
-          path='/test'
-          render={() => (
-            // @ts-ignore
-            <StripeProvider {...props} apiKey={REACT_APP_stripe_API}>
-              <MyStoreCheckout />
-            </StripeProvider>
-          )}
-        />
+        <Route exact path='/test' component={Stripe} />
         <Route exact path='/' component={LandingPage} />
         <Route path='/Login' component={Login} />
         <Route exact path='/postreg' component={PostRegister} />
