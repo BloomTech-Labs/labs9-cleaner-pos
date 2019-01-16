06ca2728dc4bf816c8a97f22ed8c067a6d9b17d9
Mister-Corn <code@ronaldlibago.com>
2019-01-15 18:59:20 -0800

    Wire Settings component to retrieve user data from backend

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,17 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import { Container, Button } from '../../components/shared_components';
 
 const Settings = () => {
-    return (
-        <Container>
-        <Button text='Update Contact' />
-            <input type='checkbox' name='email' /> I would like to receive updates via email.<br />
-            <input type='checkbox' name='text' /> I would like to receive updates via text.<br />
-        <Button text='Save' />
-        </Container>
-    );
+  // useState returns an array. first element is the value, second element is a setState function
+  const [settings, setSettings] = useState({
+    setting_email: false,
+    setting_text: false,
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = e;
+    setSettings((prev) => ({ ...prev, [target.name]: target.checked }));
+  };
+
+  useEffect(() => {
+    const headers: AxiosRequestConfig = {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    };
+
+    const url =
+      process.env.REACT_APP_backendURL || 'https://cleaner-pos.herokuapp.com';
+
+    axios
+      .get(`${url}/users`, headers)
+      .then(({ data }) => {
+        console.log(data);
+        const { setting_email, setting_text } = data;
+        setSettings({
+          setting_email,
+          setting_text,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Button text='Update Contact' />
+      <input
+        type='checkbox'
+        name='setting_email'
+        checked={settings.setting_email}
+        onChange={handleInputChange}
+      />{' '}
+      I would like to receive updates via email.
+      <br />
+      <input
+        type='checkbox'
+        name='setting_text'
+        checked={settings.setting_text}
+        onChange={handleInputChange}
+      />{' '}
+      I would like to receive updates via text.
+      <br />
+      <Button text='Save' />
+    </Container>
+  );
 };
 
 export default Settings;
