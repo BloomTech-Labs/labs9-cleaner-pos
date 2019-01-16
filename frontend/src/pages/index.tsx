2889abeff21b9d251555a9bb178c1d2ed40aa093
NandoTheessen <nando.theessen@gmail.com>
2019-01-16 19:28:25 +0100

    Add rudimentary test for stripe button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,5 +2,6 @@ import Houses from './Houses/Houses';
 import Login from './Login';
 import LandingPage from './LandingPage/Landingpage';
 import PostRegister from './PostRegister';
+import Billing from './Billing/Billing';
 
-export { Houses, Login, LandingPage, PostRegister };
+export { Houses, Login, LandingPage, PostRegister, Billing };
