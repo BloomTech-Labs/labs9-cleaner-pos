12179dd839c03de9f45f9f7fd7edf181b4f69991
Chris Honda <christopher.honda92@gmail.com>
2019-01-17 14:37:48 -0800

    Added basic styled components:

--- frontend/src/pages/Assistants/Assistants.styling.tsx
+++ frontend/src/pages/Assistants/Assistants.styling.tsx
@@ -1,1 +1,61 @@
+import styled from '@emotion/styled';
 
+const cardHeight = 216;
+
+const AssistantItem = styled('div')`
+    height: ${cardHeight}px;
+`;
+
+const ButtonContainer = styled('div')`
+    height: 124px;
+`;
+
+const ThumbNail = styled('img')`
+    width: ${cardHeight - 1}px;
+    height: ${cardHeight - 1}px;
+`;
+
+const CardHeading = styled('div')`
+    margin-top: 12px;
+`;
+
+const CardContent = styled('div')`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+const CardBody = styled('div')`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+const CheckList = styled('div')`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Asst = styled('div')`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const AssistantHeader = styled('span')`
+    text-align: left;
+`;
+
+export {
+    AssistantItem,
+    ButtonContainer,
+    ThumbNail,
+    CardHeading,
+    CardContent,
+    CardBody,
+    CheckList,
+    Asst,
+    AssistantHeader,
+};
