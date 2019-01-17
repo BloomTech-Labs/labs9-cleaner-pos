480f14fc8250f8793fab0593531a2fed70fb24ff
Chris Honda <christopher.honda92@gmail.com>
2019-01-16 20:43:01 -0800

    Map now functional - see NOTE: - Map is currently mounted on LandingPage, which breaks the style - Since component is functional, will move to its own page. - After this PR is merged, will start another to populate data from DB.

---
+++ frontend/src/components/Leaflet/Leaflet.styling.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Map } from 'react-leaflet';
+
+const CoolMap = styled(Map)`
+    height: 500px;
+`;
+
+export default CoolMap;
--- frontend/src/components/Leaflet/Leaflet.tsx
+++ frontend/src/components/Leaflet/Leaflet.tsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Container } from '../../components/index';
 import 'leaflet/dist/leaflet.css';
+import styled from '@emotion/styled';
 
+const DumbDiv = styled('div')`
+    .container-map {
+        height: 500px;
+    }
+`;
 
 const LeafletMap = () => {
     // Set state for map coordinates
@@ -12,22 +18,24 @@ const LeafletMap = () => {
         zoom: 13,
     });
 
+    const position: [number, number] = [coordinates.lat, coordinates.lng];
+
     return(
-        <Container>
-        <Map center={[coordinates.lat, coordinates.lng]} zoom={coordinates.zoom}>
-            <TileLayer
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={[coordinates.lat, coordinates.lng]}>
-                <Popup>
-                    A pretty CSS3 popup.
-                    <br />
-                    Easily customizable.
-                </Popup>
-            </Marker>
-        </Map>
-        </Container>
+        <DumbDiv>
+            <Map className='container-map' center={position} zoom={coordinates.zoom}>
+                <TileLayer
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup.
+                        <br />
+                        Easily customizable.
+                    </Popup>
+                </Marker>
+            </Map>
+        </DumbDiv>
     );
 };
 
