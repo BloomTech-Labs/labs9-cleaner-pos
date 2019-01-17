807130a6c045f9a2216f8d7d9126e4b38b5de85f
kelly <kelfro@fastmail.com>
2019-01-17 13:55:14 -0800

    setting up accordion for payments on billing page

--- frontend/src/pages/Billing/Billing.tsx
+++ frontend/src/pages/Billing/Billing.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import Stripe from './index';
 import { Container } from '../../components/index';
+import styled from '@emotion/styled';
+
+const Panel = styled('ul')`
+	list-style-type: none;
+	float:left;
+	text-align: left;
+	padding: 30px;
+	width: 300px;
+	background-color:#f1f1f1;
+`;
+
+const Li = styled('li')`
+`;
+
+const Accordion = styled('button')`
+	color: green;
+	text-align: left
+`;
+ 
+
 
 const Billing = () => {
   return (
     <Container>
-      <Stripe />
+		<div>
+			<Panel>
+				<Li><Accordion>Choose Your Plan</Accordion></Li>
+				<Li><Accordion>Enter Your Payment Information</Accordion></Li>
+				<Li><Accordion>Confirm Your Subscription</Accordion></Li>
+			</Panel>
+		</div>
+        <Stripe />
     </Container>
   );
 };
