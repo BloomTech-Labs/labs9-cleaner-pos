99ffd1fdf1a83c8dec1e305726f6b539b4f227dd
kelly <kelfro@fastmail.com>
2019-01-17 16:19:33 -0800

    rewrote the whole thing from scratch again because typescript hates me - now using React hooks

--- frontend/src/pages/Billing/Billing.tsx
+++ frontend/src/pages/Billing/Billing.tsx
@@ -1,48 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stripe from './index';
 import { Container } from '../../components/index';
 import styled from '@emotion/styled';
 
-{/* const Title = styled.h2`
-	background-color: aqua;
-	color: black;
-	padding: 15px;
-	margin-bottom: 0;
-`;
-
-const Content = styled.div`
-	opacity: ${ props => (props.open ? '1' : '0') };
-	max-height: ${ props => (props.open ? '100%' : '0') };
-	overflow: hidden;
-	padding: ${ props =P (props.open ? '15px' : '0 15px') };
-	transition: all 0.3s;
-`;
-
-class Billing extends React.Component {
-	constructor() {
-		super();
-		this.state = { open: false };
-	}
-
-toggleOpen() {
-	this.setState(prevState => ({ open: !prevState.open }));
+interface MyConfig {
+	onClick?: () => void;
+	title?: string;
+	content?: string;
 }
-*/}
+
+
+const Open = ({ onClick, title, content }: MyConfig) => {
+	const [open, setOpen] = useState('false');
+
+	return(
+		<div>
+		{open ? (
+			<a onClick={() => setOpen('true')}> {title}{content}</a>
+			) : (
+			<a onClick={() => setOpen('false')}> {title}</a>
+			)
+		}
+		</div>
+	)
+}
 const Billing = () => {
 
-
-// render() {
-  return (
-    <Container>
-{/*		<div>
-			<Title onClick = { this.toggleOpen }> First Thing </Title>
-			<Content open= { this.state.open }>
-				<p>Open Sesame!</p>
-			</Content>
-		</div>
-   */}     <Stripe />
-    </Container>
+	return(
+	    <Container>
+			<div>
+				<Open 
+					title={`first title`}
+					content={`first content`}
+				/>
+			</div>
+    	</Container>
   );
 };
+	
 export default Billing;
 
