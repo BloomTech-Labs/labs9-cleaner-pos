2d4c4b274659d58266e65f1a3870e9a867f96623
kelly <kelfro@fastmail.com>
2019-01-21 14:42:28 -0800

    I'm stuck and con't figure out where I went wrong I tried using react hooks to toggle show/hide but keep geeting TS errors

---
+++ frontend/src/pages/Billing/Billing.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Stripe from './index';
+import { Container } from '../../components/index';
+//import Example from '../../components/shared_components/Example';
+
+const Billing = () => {
+  return (
+    <Container>
+//	  <Example />
+      <Stripe />
+    </Container>
+  );
+};
+
+export default Billing;
