4ce866a1c78a86c5766b54cd2ca27c594d354e70
NandoTheessen <nando.theessen@gmail.com>
2019-01-22 00:28:50 +0100

    Add rendering of children to the accordion

---
+++ frontend/src/components/Accordion.tsx
@@ -0,0 +1,39 @@
+import React, { useState, Fragment, Children } from 'react';
+import PropTypes from 'prop-types';
+
+import styled from '@emotion/styled';
+
+interface MySettings {
+  title?: string;
+  content?: any;
+  onToggle?: (show: boolean) => void;
+  onClick?: () => void;
+  children?: any;
+}
+
+const Accordion = ({ title, children, onToggle }: MySettings) => {
+  const [show, setShow] = useState(true);
+  return (
+    <div>
+      <h2
+        onClick={() => {
+          setShow(!show);
+          if (onToggle) {
+            onToggle(!show);
+          }
+        }}
+      >
+        {title}
+      </h2>
+      {show ? <Fragment>{children}</Fragment> : null}
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  content: PropTypes.any.isRequired,
+  onToggle: PropTypes.func,
+  title: PropTypes.string.isRequired,
+};
+
+export default Accordion;
