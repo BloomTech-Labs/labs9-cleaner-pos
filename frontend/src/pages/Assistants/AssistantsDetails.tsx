3409bdd9ed7e93063d9cfa680efb25808dd09065
Chris Honda <christopher.honda92@gmail.com>
2019-01-22 17:11:32 -0800

    Added skeleton of AssistantsDetails.tsx

---
+++ frontend/src/pages/Assistants/AssistantsDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, LeafletMap } from '../../components/index';
+
+const AssistantsDetails = () => {
+    return (
+        <Container>
+            <></>
+            <LeafletMap />
+        </Container>
+    );
+};
+
+export default AssistantsDetails;
