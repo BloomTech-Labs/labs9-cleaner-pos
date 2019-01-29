37b92cd7f5e96a8f69c4f229c85898a18a47b68d
William VanDolah <william.vandolah@gmail.com>
2019-01-28 18:32:21 -0600

    returning needed data for ast detail

--- backend/src/models/assistants.ts
+++ backend/src/models/assistants.ts
@@ -53,5 +53,31 @@ export function addAstToAllManHouse(astId: number, manId: number) {
 }
 
 export function findOneAssistant(astId: number) {
-  console.log(astId);
+  return db('assistant')
+    .join('user', 'user.id', '=', 'assistant.user_id')
+    .select(
+      'user.id as user_id',
+      'assistant.id as ast_id',
+      'user.full_name as full_name',
+      'user.address as address',
+      'user.photoUrl as photo_url',
+    )
+    .where({ 'assistant.id': astId })
+    .first()
+    .then(async (e: any) => {
+      const defHouse = await db('house')
+        .select('house.id as house_id', 'house.name as house_name')
+        .where({ 'house.default_ast': astId });
+      const avlHouses = await db('house_ast')
+        .leftJoin('house', {
+          'house_ast.house_id': 'house.id',
+        })
+        .select(
+          'house.id as house_id',
+          'house.name as house_name',
+          'house_ast.id',
+        )
+        .where({ 'house_ast.ast_id': astId });
+      return { ...e, default_house: defHouse, avl_houses: avlHouses };
+    });
 }
