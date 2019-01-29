02b181b7b65df5c3d6fa20e46d39533ac158cbba
NandoTheessen <nando.theessen@gmail.com>
2019-01-29 17:52:15 +0100

    Adds useFetch hook to properties component to account for token in API call

--- frontend/src/pages/Properties/Properties.tsx
+++ frontend/src/pages/Properties/Properties.tsx
@@ -12,23 +12,15 @@ import {
   CheckList,
   HouseHeader,
 } from './Properties.styling';
-import { HousesEnum } from './types';
+import { useFetch } from '../../helpers/';
+import { HousesEnum, House } from './types';
 import { Link } from 'react-router-dom';
 
 const Properties = () => {
-  const [houses, setHouses] = useState<HousesEnum>([]);
-  const shouldFetch = useRef(true);
   const url =
     process.env.REACT_APP_backendURL || 'https://cleaner-pos.herokuapp.com';
   /* Axios calls to fetch / update properties */
-  async function fetchHouses() {
-    try {
-      const res = await axios.get(`${url}/houses`);
-      setHouses(res.data);
-    } catch (e) {
-      throw e;
-    }
-  }
+  const [houses, error, loading] = useFetch(`${url}/houses`);
 
   async function postAst(
     event: React.FormEvent<HTMLSelectElement>,
@@ -44,12 +36,6 @@ const Properties = () => {
       throw e;
     }
   }
-  // Axios call to display list of properties
-  useEffect(() => {
-    fetchHouses();
-    shouldFetch.current = false;
-  }, [shouldFetch]);
-
   // Presentational layer
   return (
     <Container>
@@ -57,62 +43,70 @@ const Properties = () => {
       <Link to='/properties/new'>
         <Button text='New Property' />
       </Link>
-      {houses.map((house) => {
-        return (
-          <HouseItem key={house.id} data-testid='house-item'>
-            <ThumbNail
-              src='https://www.samplemcdougald.org/wp-content/uploads/2017/10/visit-sample-mcdougald-300x300.jpg'
-              alt='house'
-            />
-            <CardContent>
-              <CardHeading>
-                <h4>{house.name}</h4>
-                <p>{house.address}</p>
-              </CardHeading>
-              <CardBody>
-                <CheckList>
-                  <p>Checklist Items</p>
-                  {house.checkList[0].count}
-                </CheckList>
-                <ButtonContainer>
-                  <Link
-                    to={{
-                      pathname: `properties/${house.id}`,
-                      hash: '#checklists',
-                      state: house,
-                    }}
-                  >
-                    <Button text='Edit Checklists' datatestid='house-button' />
-                  </Link>
-                  <Link to={`/houses/${house.id}#resources`}>
-                    <Button text='Edit Resources' datatestid='house-button' />
-                  </Link>
-                </ButtonContainer>
-                <Cleaner>
-                  Default Cleaner
-                  <select
-                    data-testid='assistant-select'
-                    onChange={(event) => postAst(event, house.id)}
-                  >
-                    <option defaultValue={house.default_ast_name}>
-                      {house.default_ast}: {house.default_ast_name}
-                    </option>
-                    {house.openAst.map((ast: any) => {
-                      if (ast.ast_id !== house.default_ast) {
-                        return (
-                          <option key={ast.ast_id}>
-                            {ast.ast_id}: {ast.full_name}
-                          </option>
-                        );
-                      }
-                    })}
-                  </select>
-                </Cleaner>
-              </CardBody>
-            </CardContent>
-          </HouseItem>
-        );
-      })}
+      {houses
+        ? houses.map((house: House) => {
+            return (
+              <HouseItem key={house.id} data-testid='house-item'>
+                <ThumbNail
+                  src='https://www.samplemcdougald.org/wp-content/uploads/2017/10/visit-sample-mcdougald-300x300.jpg'
+                  alt='house'
+                />
+                <CardContent>
+                  <CardHeading>
+                    <h4>{house.name}</h4>
+                    <p>{house.address}</p>
+                  </CardHeading>
+                  <CardBody>
+                    <CheckList>
+                      <p>Checklist Items</p>
+                      {house.checkList[0].count}
+                    </CheckList>
+                    <ButtonContainer>
+                      <Link
+                        to={{
+                          pathname: `properties/${house.id}`,
+                          hash: '#checklists',
+                          state: house,
+                        }}
+                      >
+                        <Button
+                          text='Edit Checklists'
+                          datatestid='house-button'
+                        />
+                      </Link>
+                      <Link to={`/houses/${house.id}#resources`}>
+                        <Button
+                          text='Edit Resources'
+                          datatestid='house-button'
+                        />
+                      </Link>
+                    </ButtonContainer>
+                    <Cleaner>
+                      Default Cleaner
+                      <select
+                        data-testid='assistant-select'
+                        onChange={(event) => postAst(event, house.id)}
+                      >
+                        <option defaultValue={house.default_ast_name}>
+                          {house.default_ast}: {house.default_ast_name}
+                        </option>
+                        {house.openAst.map((ast: any) => {
+                          if (ast.ast_id !== house.default_ast) {
+                            return (
+                              <option key={ast.ast_id}>
+                                {ast.ast_id}: {ast.full_name}
+                              </option>
+                            );
+                          }
+                        })}
+                      </select>
+                    </Cleaner>
+                  </CardBody>
+                </CardContent>
+              </HouseItem>
+            );
+          })
+        : null}
     </Container>
   );
 };
