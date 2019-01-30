e3407232e787a2aad2080a76cc90cac330999a04
Chris Honda <christopher.honda92@gmail.com>
2019-01-30 13:23:11 -0800

    Icons now render appropriately based on coordinates

--- frontend/src/components/Leaflet/Leaflet.styling.tsx
+++ frontend/src/components/Leaflet/Leaflet.styling.tsx
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
 
-
 // The class .container-map with defined height is essential to have map rendered
 
 const MapDiv = styled('div')`
     .container-map {
         height: 500px;
+        width: 500px;
+    }
+    .leaflet-container {
+        height: 400px;
+        width: 400px;
     }
 `;
 
--- frontend/src/components/Leaflet/Leaflet.tsx
+++ frontend/src/components/Leaflet/Leaflet.tsx
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import MapDiv from './Leaflet.styling';
 import 'leaflet/dist/leaflet.css';
+import L, { LatLngExpression } from 'leaflet';
+import Pointer from './33622.svg';
+
+const iconPerson: any = L.icon({
+  iconUrl: Pointer,
+  // iconRetinaUrl: null,
+  iconAnchor: [20, 40],
+  // popupAnchor: null,
+  // shadowUrl: Pointer,
+  // shadowSize: null,
+  // shadowAnchor: [60, 75],
+  iconSize: [60, 40],
+  // className: 'leaflet-div-icon',
+});
+
+export { iconPerson };
 
 const LeafletMap = (props: { className?: string }) => {
   // Set state for map coordinates
@@ -10,22 +27,29 @@ const LeafletMap = (props: { className?: string }) => {
     zoom: 15,
   });
 
+  const Positions = [[38.6953, -121.0137], [38.695394, -121.013766]];
   const position: [number, number] = [coordinates.lat, coordinates.lng];
 
   return (
-    <Map className='container-map' center={position} zoom={coordinates.zoom}>
-      <TileLayer
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup.
-          <br />
-          Easily customizable.
-        </Popup>
-      </Marker>
-    </Map>
+    <MapDiv>
+      <Map className='container-map' center={position} zoom={coordinates.zoom}>
+        <TileLayer
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {Positions.map((latlng: any) => {
+          return (
+          <Marker position={latlng} icon={iconPerson}>
+            <Popup>
+              A pretty CSS3 popup.
+              <br />
+              Easily customizable.
+            </Popup>
+          </Marker>
+          );
+        })}
+      </Map>
+    </MapDiv>
   );
 };
 
