a1c95177b6a3406028a4828a458da575d2827be0
NandoTheessen <nando.theessen@gmail.com>
2019-01-31 20:01:08 +0100

    Remove console.log

--- frontend/src/components/Leaflet/Leaflet.tsx
+++ frontend/src/components/Leaflet/Leaflet.tsx
@@ -9,11 +9,8 @@ import Pointer from './33622.svg';
 const Geocode = require('react-geocode');
 
 Geocode.default.setApiKey(process.env.REACT_APP_GEO);
-console.log(Geocode);
 
 Geocode.default.enableDebug();
-
-
 
 const iconPerson: any = L.icon({
   iconUrl: Pointer,
@@ -40,34 +37,35 @@ const LeafletMap = (props: { className?: string }) => {
   const Positions = [[38.6953, -121.0137], [38.695394, -121.013766]];
   const position: [number, number] = [coordinates.lat, coordinates.lng];
   if (false) {
-    Geocode.default.fromAddress('1600 Ampitheatre Parkway, Mountain View, CA')
-    .then((response: any) => {
-      const { lat, lng } = response.results[0].geometry.location;
-      console.log(lat, lng);
-    })
-    .catch((error: any) => {
-      console.error(error);
-    });
+    Geocode.default
+      .fromAddress('1600 Ampitheatre Parkway, Mountain View, CA')
+      .then((response: any) => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log(lat, lng);
+      })
+      .catch((error: any) => {
+        console.error(error);
+      });
   }
 
   return (
-      <Map className='container-map' center={position} zoom={coordinates.zoom}>
-        <TileLayer
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {Positions.map((latlng: any) => {
-          return (
-            <Marker position={latlng} icon={iconPerson}>
-              <Popup>
-                A pretty CSS3 popup.
-                <br />
-                Easily customizable.
-              </Popup>
-            </Marker>
-          );
-        })}
-      </Map>
+    <Map className='container-map' center={position} zoom={coordinates.zoom}>
+      <TileLayer
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {Positions.map((latlng: any) => {
+        return (
+          <Marker position={latlng} icon={iconPerson}>
+            <Popup>
+              A pretty CSS3 popup.
+              <br />
+              Easily customizable.
+            </Popup>
+          </Marker>
+        );
+      })}
+    </Map>
   );
 };
 
