a46b32ab8ff445d5686c3b71b4b0e6d8ba8216fc
Chris Honda <christopher.honda92@gmail.com>
2019-02-04 10:41:39 -0800

    More logo formatting

--- frontend/src/pages/LandingPage/Landingpage.tsx
+++ frontend/src/pages/LandingPage/Landingpage.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 import { Button, Container } from '../../components/index';
 import house from '../../assets/house.jpg';
+import lodgel from '../../assets/lodgel.png';
 import {
   Footer,
   AppHeader,
@@ -10,17 +11,16 @@ import {
   Wrapper,
   Nav,
   CTA,
+  BigLogo,
+  Logo,
 } from './LandingPage.styling';
 
 const Landing = (props: RouteComponentProps) => {
   return (
     <Container>
-      <AppHeader>Lodgel</AppHeader>
       {/* <h2>A POS that helps you keep things clean</h2> */}
       <Nav>
-        <a href='/'>Home</a>
-        <a href='/'>About</a>
-        <a href='/'>Contact</a>
+        <BigLogo src={lodgel} alt='Lodgel logo' />
         <Link to='/Login'>
           <Button text='Sign In' />
         </Link>
@@ -39,7 +39,7 @@ const Landing = (props: RouteComponentProps) => {
         <h2>Simplify managing your rental properties.</h2>
       </CTA>
         <Footer>
-          <a href='/'>POS</a>
+          <a href='/'><Logo src={lodgel} alt='Lodgel logo' /></a>
           <a href='https://example.com'>Email</a>
           <a href='https://twitter.com'>Twitter</a>
           <a href='https://github.com/Lambda-School-Labs/labs9-cleaner-pos'>
