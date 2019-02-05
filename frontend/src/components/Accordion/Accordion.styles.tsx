383218df387d5dfafdc5f7a6bc14dc052789673e
NandoTheessen <nando.theessen@gmail.com>
2019-02-05 23:56:02 +0100

    (UI) Changes accordion to assume 100% of it's parents height

--- frontend/src/components/Accordion/Accordion.styles.tsx
+++ frontend/src/components/Accordion/Accordion.styles.tsx
@@ -2,16 +2,21 @@ import styled from '@emotion/styled';
 
 export const AccordionItemHeader = styled('dt')`
   background: var(--color-bg-main);
-  border: var(--border);
+  border-bottom: var(--border);
   padding: 0.25rem 0.75rem;
   text-align: left;
 `;
 export const AccordionItemBody = styled('dd')`
   text-align: left;
-  padding: 0.75rem;
+  padding: 0.5rem;
   background: var(--color-main-light);
   margin: 0;
+  height: calc(100% - 126px);
 `;
 export const AccordionContainer = styled('dl')`
   border: var(--border);
+  margin: 0;
+  width: 50%;
+  min-width: 350px;
+  height: 100%;
 `;
