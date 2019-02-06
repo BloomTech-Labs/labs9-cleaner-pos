d5968f26b8c6c3b0ad2d9e060d30ecda2b56c86b
NandoTheessen <nando.theessen@gmail.com>
2019-02-06 17:29:31 +0100

    Global changes to styles, adds top margin for container changes sidebar buttons

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from 'react';
-import { UserContext } from '../../App';
+import React, { useState } from 'react';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Mark from '@material-ui/core/Button';
@@ -43,119 +42,133 @@ const Sidebar = (props: LinkProps) => {
   };
 
   return (
-    <Container {...props}>
-      <div className='menu'>
-        <Mark
-          aria-owns={anchorEl ? 'simple-menu' : undefined}
-          aria-haspopup='true'
-          onClick={handleClick}
-        >
-          Menu
-        </Mark>
-        <Menu
-          id='simple-menu'
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleClose}
-        >
-          <MenuItem onClick={handleClose}>
-            <Link to='/properties'>Property</Link>
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Link to='/guests'>Guests</Link>
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Link to='/assistants'>Assistants</Link>
-          </MenuItem>
-        </Menu>
-        <SettingsWrapper>
-          <StyledLink to='/settings'>
-            <i className='fas fa-cog' />
-          </StyledLink>
-          <SpecialButton
-            onClick={() => logOut()}
-            className='fas fa-sign-out-alt signout'
-          />
-        </SettingsWrapper>
-      </div>
-      <NavBar {...props}>
-        <NavWrapper>
-          <StyledUL>
-            {/* Uncomment this section when reports page is complete */}
-            {/* <div>
+    <div
+      style={{
+        maxWidth: '9000px',
+        width: '100%',
+        borderBottom: '3px solid var(--color-border-strong)',
+        background: 'white',
+      }}
+    >
+      <Container {...props}>
+        <div className='menu'>
+          <Mark
+            aria-owns={anchorEl ? 'simple-menu' : undefined}
+            aria-haspopup='true'
+            onClick={handleClick}
+          >
+            Menu
+          </Mark>
+          <Menu
+            id='simple-menu'
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>
+              <Link to='/properties'>Property</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to='/guests'>Guests</Link>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Link to='/assistants'>Assistants</Link>
+            </MenuItem>
+          </Menu>
+          <SettingsWrapper>
+            <StyledLink to='/settings'>
+              <i className='fas fa-cog' />
+            </StyledLink>
+            <SpecialButton
+              onClick={() => logOut()}
+              className='fas fa-sign-out-alt signout'
+            />
+          </SettingsWrapper>
+        </div>
+        <NavBar {...props}>
+          <NavWrapper>
+            <StyledUL>
+              {/* Uncomment this section when reports page is complete */}
+              {/* <div>
               <StyledLink to='/reports'>
                 <h4>Reports</h4>
               </StyledLink>
             </div> */}
-            <div>
-              <StyledLink to='/' className='header-bar--logo'>
-                <img
-                  style={{ height: '100px', width: '100px' }}
-                  src={logo}
-                  alt='Lodgel logo'
-                />
-              </StyledLink>
-            </div>
-            <div>
-              <StyledLink to='/properties'>
-                <h4
-                  style={{
-                    borderBottom: props.location.pathname.match(
-                      '/properties/*.*?',
-                    )
-                      ? '2px solid var(--color-accent-alt)'
-                      : '0',
-                  }}
-                >
-                  Properties
-                </h4>
-              </StyledLink>
-            </div>
-            <div>
-              <StyledLink to='/guests'>
-                <h4
-                  style={{
-                    borderBottom: props.location.pathname.match('/guests/*.*?')
-                      ? '2px solid var(--color-accent-alt)'
-                      : '0',
-                  }}
-                >
-                  Guests
-                </h4>
-              </StyledLink>
-            </div>
-            <div>
-              <StyledLink to='/assistants'>
-                <h4
-                  style={{
-                    borderBottom: props.location.pathname.match(
-                      '/assistants/*.*?',
-                    )
-                      ? '2px solid var(--color-accent-alt)'
-                      : '0',
-                  }}
-                >
-                  Assistants
-                </h4>
-              </StyledLink>
-            </div>
-          </StyledUL>
-        </NavWrapper>
-        <SettingsWrapper>
-          <Link to='/billing'>
-            <Button text='Subscribe' className='header-bar--subscribe-button' />
-          </Link>
-          <StyledLink to='/settings'>
-            <i className='fas fa-cog' />
-          </StyledLink>
-          <SpecialButton
-            type='button'
-            onClick={() => logOut()}
-            className='fas fa-sign-out-alt fa-lg signout'
-          />
-        </SettingsWrapper>
-      </NavBar>
-    </Container>
+              <div>
+                <StyledLink to='/' className='header-bar--logo'>
+                  <img
+                    style={{ height: '100px', width: '100px' }}
+                    src={logo}
+                    alt='Lodgel logo'
+                  />
+                </StyledLink>
+              </div>
+              <div>
+                <StyledLink to='/properties'>
+                  <h4
+                    style={{
+                      borderBottom: props.location.pathname.match(
+                        '/properties/*.*?',
+                      )
+                        ? '2px solid var(--color-accent-alt)'
+                        : '0',
+                    }}
+                  >
+                    Properties
+                  </h4>
+                </StyledLink>
+              </div>
+              <div>
+                <StyledLink to='/guests'>
+                  <h4
+                    style={{
+                      borderBottom: props.location.pathname.match(
+                        '/guests/*.*?',
+                      )
+                        ? '2px solid var(--color-accent-alt)'
+                        : '0',
+                    }}
+                  >
+                    Guests
+                  </h4>
+                </StyledLink>
+              </div>
+              <div>
+                <StyledLink to='/assistants'>
+                  <h4
+                    style={{
+                      borderBottom: props.location.pathname.match(
+                        '/assistants/*.*?',
+                      )
+                        ? '2px solid var(--color-accent-alt)'
+                        : '0',
+                    }}
+                  >
+                    Assistants
+                  </h4>
+                </StyledLink>
+              </div>
+            </StyledUL>
+          </NavWrapper>
+          <SettingsWrapper>
+            <Link to='/billing'>
+              <Button
+                text='Subscribe'
+                className='header-bar--subscribe-button'
+              />
+            </Link>
+            <StyledLink to='/settings'>
+              <i className='fas fa-cog' />
+            </StyledLink>
+            <SpecialButton
+              type='button'
+              onClick={() => logOut()}
+              className='fas fa-sign-out-alt fa-lg signout'
+            />
+          </SettingsWrapper>
+        </NavBar>
+      </Container>
+    </div>
   );
 };
 
