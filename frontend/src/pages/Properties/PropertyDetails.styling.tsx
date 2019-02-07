b0da48fd418bf3103857ff78d56f6ca175d317ea
kelly <kelfro@fastmail.com>
2019-02-07 10:05:32 -0800

    deleted unnecessary imports, updated styling so that there is padding on all sizes of Property Details page

--- frontend/src/pages/Properties/PropertyDetails.styling.tsx
+++ frontend/src/pages/Properties/PropertyDetails.styling.tsx
@@ -3,25 +3,21 @@ import { Container, Button } from '../../components/index';
 
 const pxToRem = (px: number) => px / 16;
 const cardHeight = pxToRem(84);
-const cardWidth = pxToRem(120);
+const cardWeidth = pxToRem(120);
 
 export const BackButton = styled(Button)`
   width: ${pxToRem(156)}rem;
   height: ${pxToRem(48)}rem;
   font-size: ${pxToRem(20)}rem;
-  /* border: 0.25px solid black; */
-  border: var(--border);
-  @media only screen and (max-width: 900px) {
-  }
+	border: 0.25px solid black;
+	@media only screen and (max-width: 900px) {
+	}
 `;
 
-export const PropertyButtons = styled('div')``;
-
 export const WhiteButton = styled(Button)`
-  color: var(--color-button-text-alt);
-  background-color: var(--color-button-text-action);
-  /* border: solid #707070 1px; */
-  border: var(--border);
+  color: var(--colour-button-text-alt);
+  background-color: var(--colour-button-background-alt);
+  border: solid #707070 1px;
   width: ${pxToRem(161)}rem;
   height: ${pxToRem(36)}rem;
   font-size: ${pxToRem(16)}rem;
@@ -33,6 +29,7 @@ export const PropertyContainer = styled(Container)`
   justify-content: flex-start;
   align-items: flex-start;
   max-width: 100%;
+	padding: 15px;
 `;
 
 export const Top = styled(PropertyContainer)`
@@ -40,31 +37,25 @@ export const Top = styled(PropertyContainer)`
   width: 100%;
   display: flex;
   flex-flow: row nowrap;
-  justify-content: space-between;
-  align-items: center;
+	justify-content: space-between;
+	align-items: center;
   /* Text */
   text-align: left;
   @media only screen and (max-width: 900px) {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    width: 100%;
-  }
-  @media only screen and (max-width: 400px) {
-    flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		width: 100%;
   }
 `;
 
-export const HouseInfo = styled('div')``;
-
 export const ThumbNail = styled('img')`
-  width: ${cardWidth}rem;
+  width: ${cardWeidth}rem;
   height: ${cardHeight}rem;
   object-fit: cover;
   @media only screen and (max-width: 900px) {
-    margin: 0 auto;
-    padding: 10px;
+  	margin: 0 auto;
+	  padding: 10px;
   }
 `;
 export const MainText = styled.div`
@@ -72,17 +63,17 @@ export const MainText = styled.div`
   font-family: 'Roboto Bold', Arial, sans-serif;
   font-size: ${pxToRem(36)}rem;
   @media only screen and (max-width: 900px) {
-    width: 100%;
+	  width: 100%;
   }
 `;
 
 export const SecondaryText = styled.div`
   font-family: 'Roboto Bold', Arial, sans-serif;
-  font-size: ${pxToRem(20)}rem;
+  font-size: ${pxToRem(24)}rem;
   @media only screen and (max-width: 900px) {
-    width: 100%;
-    text-align: center;
-    padding-bottom: 20px;
+		width: 100%;
+		text-align: center;
+		padding-bottom: 20px;
   }
 `;
 
@@ -90,7 +81,7 @@ export const Header = styled('div')`
   width: 100%;
   padding: ${pxToRem(16)}rem ${pxToRem(28)}rem;
   height: ${pxToRem(60)}rem;
-  background: var(--color-text-accent);
+  background: var(--colour-accent);
   text-align: left;
   color: white;
   font-family: 'Roboto', Arial, sans-serif;
@@ -103,19 +94,16 @@ export const ListContainer = styled('div')`
   justify-content: space-between;
   flex-flow: row wrap;
   @media only screen and (max-width: 900px) {
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  	flex-direction: column;
+		justify-content: center;
+		align-items: center;
   }
 `;
 export const ListDiv = styled('div')`
   width: 45%;
   min-width: ${pxToRem(254)}rem;
-  /* border: solid #707070 1px; */
-  border: var(--border);
+  border: solid #707070 1px;
   margin: ${pxToRem(36)}rem 0;
-  display: flex;
-  flex-direction: column;
 `;
 
 export const ItemDiv = styled('div')`
@@ -123,9 +111,7 @@ export const ItemDiv = styled('div')`
   margin: ${pxToRem(24)}rem ${pxToRem(28)}rem;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  background-color: var(--color-bg-secondary);
-  flex-grow: 1;
+  background-color: var(--colour-button-background-alt);
 `;
 
 export const TaskDiv = styled('div')`
@@ -133,25 +119,14 @@ export const TaskDiv = styled('div')`
   flex-direction: column;
   align-items: flex-start;
 
-  .listMap {
-    width: 100%;
-  }
-
-  .task {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  div {
+    margin: ${pxToRem(8)}rem 0;
+    font-size: ${pxToRem(20)}rem;
+    font-family: 'Roboto Light', Arial, sans-serif;
   }
 `;
 
 // after List
-export const AfterStay = styled('div')`
-  width: 100%;
-  @media only screen and (max-width: 900px) {
-    width: 70%;
-    margin: 0 auto;
-  }
-`;
 
 export const AfterListDiv = styled(ListDiv)`
   display: flex;
@@ -159,47 +134,21 @@ export const AfterListDiv = styled(ListDiv)`
   align-items: top;
   width: 100%;
   margin: ${pxToRem(36)}rem 0;
-  @media only screen and (max-width: 600px) {
-    flex-wrap: nowrap;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
 `;
 
 export const AfterListDiv2 = styled(ListDiv)`
   margin: ${pxToRem(36)}rem ${pxToRem(28)}rem;
-  width: ${pxToRem(255)}rem;
-  @media only screen and (max-width: 400px) {
-    border: none;
-  }
+  width: ${pxToRem(254)}rem;
 `;
 
 export const AfterHeader = styled(Header)`
   font-size: ${pxToRem(20)}rem;
-  background: var(--color-bg-main);
-  color: var(--color-button-text-alt);
-  border-bottom: solid #707070 1px;
-  justify-content: flex-start;
-  @media only screen and (max-width: 400px) {
-    max-width: 90%;
-    align-self: centered;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-  }
+  background: var(--colour-main-background);
+  color: var(--colour-button-text-alt);
+  border: solid #707070 1px;
 `;
 
 export const AfterItemDiv = styled(ItemDiv)`
   padding: ${pxToRem(24)}rem ${pxToRem(28)}rem;
   margin: 0;
-  @media only screen and (max-width: 400px) {
-    max-width: 90%;
-    align-self: centered;
-    margin-left: auto;
-    margin-right: auto;
-    width: 100%;
-    /* border: solid #707070 1px; */
-    border: var(--border);
-  }
 `;
