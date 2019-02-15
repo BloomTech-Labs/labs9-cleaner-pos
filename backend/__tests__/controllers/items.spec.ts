3f95cd893f0c23e2d5e062819627fbcdd244c3b2
NandoTheessen <nando.theessen@gmail.com>
2019-02-15 15:32:42 +0100

    (chore) Fixes & updates
    - Updates users controller tests to use async await syntax
    - Adds catch statements for items controller tests

--- backend/__tests__/controllers/items.spec.ts
+++ backend/__tests__/controllers/items.spec.ts
@@ -42,11 +42,21 @@ describe('/item routes', () => {
   });
 
   test('Unable to get all lists without token', async () => {
-    await supertest.get('/items').expect(403);
+    await supertest
+      .get('/items')
+      .expect(403)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('Unable to get single lists without token', async () => {
-    await supertest.get('/items/1').expect(403);
+    await supertest
+      .get('/items/1')
+      .expect(403)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('Unable to post without token', async () => {
@@ -57,11 +67,19 @@ describe('/item routes', () => {
     await supertest
       .post('/items/')
       .send(newItem)
-      .expect(403);
+      .expect(403)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('Unable to delete item without token', async () => {
-    await supertest.delete('/items/5').expect(403);
+    await supertest
+      .delete('/items/5')
+      .expect(403)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('Unable to put item without token', async () => {
@@ -72,14 +90,20 @@ describe('/item routes', () => {
     supertest
       .put('/items/599')
       .send(newItem)
-      .expect(403);
+      .expect(403)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('Able to get all lists', async () => {
     const { body } = await supertest
       .get('/items')
       .set(headers)
-      .expect(200);
+      .expect(200)
+      .catch((e) => {
+        throw e;
+      });
 
     expect(body).toHaveProperty('length');
     expect(Object.keys(body[0])).toHaveLength(3);
@@ -89,7 +113,10 @@ describe('/item routes', () => {
     const { body } = await supertest
       .get('/items/1')
       .set(headers)
-      .expect(200);
+      .expect(200)
+      .catch((e) => {
+        throw e;
+      });
     expect(body).toHaveProperty('length');
     expect(Object.keys(body[0])).toHaveLength(3);
   });
@@ -98,7 +125,10 @@ describe('/item routes', () => {
     await supertest
       .get('/items/199')
       .set(headers)
-      .expect(404);
+      .expect(404)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('unable to post if valid List id isnt given', async () => {
@@ -110,14 +140,20 @@ describe('/item routes', () => {
       .post('/items/')
       .send(newItem)
       .set(headers)
-      .expect(400);
+      .expect(400)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('able to delete item', async () => {
     await supertest
       .delete('/items/5')
       .set(headers)
-      .expect(200);
+      .expect(200)
+      .catch((e) => {
+        throw e;
+      });
   });
 
   test('unable to put item when invalid list_id is given', async () => {
@@ -129,6 +165,9 @@ describe('/item routes', () => {
       .put('/items/599')
       .set(headers)
       .send(newItem)
-      .expect(400);
+      .expect(400)
+      .catch((e) => {
+        throw e;
+      });
   });
 });
